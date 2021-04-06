1d848fd499a5cc0b2f3bf27f43f070df2c688880
Paolo Venturi <paolo.venturi@cgm.com>
2021-04-06 17:03:27 +0200

    Caricamento heroes fatto tramite un servizio

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,13 +9,22 @@ import { HEROES } from '../mock-heroes';
 })
 
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  }
+
+  constructor(private heroService: HeroService) {
+
+  }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
   }
 }
